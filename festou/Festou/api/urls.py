a0b3027a116d06d5487312b71e541c385cd455e7
Vinicius Lazzari <vinilazzari028@gmail.com>
2023-08-18 16:20:45 -0300

    feat: remove unused path

--- festou/Festou/api/urls.py
+++ festou/Festou/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('user', views.UserView.as_view()),
     path('v1/signup', views.CreateUserView.as_view()),
     path('v1/login', views.LoginUserView.as_view()),
     path('v1/search', views.PlaceSearchView.as_view()),

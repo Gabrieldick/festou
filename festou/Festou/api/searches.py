116f790857f90eb94e0c78a6922568d8d6e341d1
Gabriel_Dick <gabrieldick036@gmail.com>
2023-08-23 11:21:02 -0300

    Imagens enviadas em search_place também

--- festou/Festou/api/searches.py
+++ festou/Festou/api/searches.py
@@ -75,7 +75,36 @@ def place(self, request):
 
         serializer = PlaceSerializer(places, many=True)
 
-        return Response(serializer.data, status=200)
+         # Lista para armazenar os lugares com imagens em base64
+        places_with_images_base64 = []
+
+        for place in places:
+
+            # Obtenha os caminhos dos arquivos de imagem dos campos do modelo
+            image_1_path = place.image_1.path if place.image_1 else ''
+            image_2_path = place.image_2.path if place.image_2 else ''
+            image_3_path = place.image_3.path if place.image_3 else ''
+
+            # Leitura dos dados das imagens (assumindo que sejam arquivos locais)
+            image_1_data = open(image_1_path, 'rb').read() if image_1_path else None
+            image_2_data = open(image_2_path, 'rb').read() if image_2_path else None
+            image_3_data = open(image_3_path, 'rb').read() if image_3_path else None
+
+            # Codifique as imagens em base64 para incluí-las no JSON
+            image_1_base64 = base64.b64encode(image_1_data).decode('utf-8') if image_1_data else ''
+            image_2_base64 = base64.b64encode(image_2_data).decode('utf-8') if image_2_data else ''
+            image_3_base64 = base64.b64encode(image_3_data).decode('utf-8') if image_3_data else ''
+
+            # Adicione as imagens aos dados de resposta
+            response_data = PlaceSerializer(place).data
+            response_data['image_1'] = image_1_base64
+            response_data['image_2'] = image_2_base64
+            response_data['image_3'] = image_3_base64
+            
+            # Adicione o lugar modificado à lista
+            places_with_images_base64.append(response_data)
+
+        return Response(places_with_images_base64, status=200)
     return Response({'description': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
 
 def id_place_list(self, request):
